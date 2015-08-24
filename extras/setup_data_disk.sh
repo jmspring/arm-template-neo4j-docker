e5ea34ed7caa03483da9f31dee80d36ca2d5743e
Jim Spring <jmspring@gmail.com>
2015-08-24 15:08:04 -0700

    adding a test script to test w/ ARM script

---
+++ extras/setup_data_disk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is necessary to set up the data disk that will
+# be passed into the Neo4j docker instance and used as the 
+# mount point for the Neo4J data.
+
+echo "this is a test" > /tmp/provtest1
